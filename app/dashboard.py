a65e0cf10920a612fd56ee1b02eb8e409db200fb
DLS Nadavi <dlsnadavi2021@gmail.com>
2026-08-08 18:06:26 +0530

    dashboard.py

--- app/dashboard.py
+++ app/dashboard.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 import calendar
 import plotly.express as px
-from pathlib import Path
+import joblib
 
+from pathlib import Path
 
 # ============================================================
 # PAGE CONFIGURATION
